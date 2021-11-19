761465d63b4a160c4083168d8bfd9c09468cd1f4
Juandominguez2311 <57020357+Juandominguez2311@users.noreply.github.com>
2021-11-19 09:19:58 -0300

    Update cart.component.ts

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -65,7 +65,6 @@ export class CartComponent implements OnInit {
       this.total += (element.price*element.quantity);
       localStorage.setItem('total',this.total.toString())
     })
-     window.location.reload();
   }
   deleteMethod(productId:number) {
     if(confirm("Esta seguro de sacar el producto? ")) {
